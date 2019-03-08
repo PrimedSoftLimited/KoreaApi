376443954402e9f89580d2dbd521e0dff8a94581
gabrieltino <miracle8093@gmail.com>
2019-03-08 20:08:43 +0100

    added more user fields

--- database/migrations/2019_03_05_094100_create_users_table.php
+++ database/migrations/2019_03_05_094100_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('username')->unique();
             $table->string('email')->unique();
-            $table->integer('phone')->unique();
+            $table->bigInteger('phone')->unique();
             $table->string('password');
             $table->string('api_token', 80)
             ->unique()
